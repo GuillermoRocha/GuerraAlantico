0ed9266352c687c84f39f0dea8fc93de9a6139fe
Guillermo Rocha <guillermo.rocha@mercadolibre.com>
2022-03-07 20:52:08 -0300

    se modifica formato de respuesta de las fechas de PArtidaDTO

--- src/main/java/com/example/guerraalantico/DTO/PartidaDTO.java
+++ src/main/java/com/example/guerraalantico/DTO/PartidaDTO.java
@@ -4,10 +4,13 @@ import java.sql.Date;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -16,7 +19,9 @@ import lombok.Setter;
 public class PartidaDTO {
 
   private Integer idPartida;
+  @JsonFormat(pattern="dd-MM-yyyy")
   private Date fechaAlta;
+  @JsonFormat(pattern="dd-MM-yyyy")
   private Date fechaGuardado;
   private boolean finalizada;
   private String jugadorRival;
